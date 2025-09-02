a0fbb10f8c747ef4e36222ca61e794b1045f0b3b
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2025-09-02 18:12:01 -0300

    add git aliases; add branch in prompt

--- .bashrc
+++ .bashrc
@@ -2,11 +2,12 @@
 
 alias dot="code ~/dotfiles"
 
-export EDITOR=vim
-export LIBVIRT_DEFAULT_URI="qemu:///system"
-export PS1="${PS1%\$}\t\n\$ "
-export TZ="America/Sao_Paulo"
-
 source ~/dotfiles/system/files/shell.sh
 source ~/dotfiles/dev/git/shell.sh
 source ~/dotfiles/dev/node/shell.sh
+
+export EDITOR=vim
+export LIBVIRT_DEFAULT_URI="qemu:///system"
+
+export PS1="\[\033[01;32m\]\u \[\033[01;34m\]\w\[\033[01;33m\] $(gbranch)\[\033[00m\] \t\n"
+export TZ="America/Sao_Paulo"
--- dev/git/shell.sh
+++ dev/git/shell.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 # git shell env
 
-alias ga="git add -A"
+alias ga="git add"
+alias gaa="git add -A"
+alias gb="git branch"
 alias gc="git add -A && git commit -m"
-alias gcm="git add -A && git commit"
-alias gps="git push"
+alias gcm="git commit"
+alias gco="git checkout"
+alias gcob="git checkout -b"
+alias gcred="git config credential.helper store"
+alias gl="git log"
 alias gpl="git pull"
+alias gps="git push"
+alias grh="git reset --hard"
 alias grhh="git reset --hard HEAD^"
+alias grs="git reset --soft"
 alias grsh="git reset --soft HEAD^"
 alias gs="git status"
-alias gsc="git config credential.helper store"
-alias gclean="git switch main && git branch | grep -v main | xargs git branch -d"
+
+gbranch() {
+  # get current git branch
+  git branch 2>/dev/null | sed -n '/\* /s///p'
+}
+
+gclean() {
+  # delete local branches except main
+  git switch main && git branch | grep -v main | xargs git branch -d
+}

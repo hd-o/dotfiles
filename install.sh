8f3f548cf85491d67012001ed26b9bbbd319fccc
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2026-09-22 22:24:17 +0000

    fix install

--- install.sh
+++ install.sh
@@ -2,16 +2,14 @@
 
 echo "source ~/dotfiles/.bashrc" >> ~/.bashrc
 
+# CLI tools, GUI testing
 sudo apt update && sudo apt install -y \
-  # CLI tools
   bat fd-find fzf jq ripgrep tree vim xdg-utils \
-  # GUI testing
   xvfb x11-utils scrot xdotool
 
 git config --global core.editor vim
 git config --global user.name "Hadrian de Oliveira"
 (cd ~/dotfiles && git config user.email "hadrian.no.reply@outlook.com")
 
-~/dotfiles/dev/nvm/install.sh
 ~/dotfiles/dev/opencode/install.sh
 ~/dotfiles/dev/config/install.sh

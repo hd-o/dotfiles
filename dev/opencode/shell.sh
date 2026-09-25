ea7b36530d20875b873bb32f92140c87c1836dd5
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2026-09-25 20:23:49 -0300

    add opencode alias

--- dev/opencode/shell.sh
+++ dev/opencode/shell.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # opencode shell env
 
+export OC_CFG="$HOME/.config/opencode/opencode.jsonc"
+
 alias oc="opencode"
-alias occ="vim $HOME/.config/opencode/opencode.jsonc"
+alias ocv="vim $OC_CFG"
+alias occ="code $OC_CFG"
